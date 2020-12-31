5ffecd03124bec0a18c5781ba3ec2bd55e555637
“REA <me@example.com>
2020-12-31 20:54:11 +1100

    install sass package, include basic styling for property card component, render a column for results and saved properties

---
+++ src/components/PropertyCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styling/propertyCard.scss";
+
+const PropertyCard = ({ property }) => {
+  const { id, mainImage, price, agency } = property;
+  console.log(property);
+  return (
+    <div className="property-card">
+      <div
+        className="property-card__header"
+        style={{ background: agency.brandingColors.primary }}
+      >
+        <img src={agency.logo} alt="" />
+      </div>
+      <div className="property-card__body">
+        <img src={mainImage} alt="property image" />
+      </div>
+      <div className="property-card__footer">Price: {price}</div>
+    </div>
+  );
+};
+
+export default PropertyCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { usePropertyContext } from "../context/property_context";
+import PropertyCard from "../components/PropertyCard";
+import "../styling/home.scss";
 
 const HomePage = () => {
   const { results, savedProperties, removeProperty } = usePropertyContext();
-  console.log(results);
-  console.log(savedProperties);
   return (
-    <main>
-      <h1>Home pages</h1>
-      <button onClick={() => removeProperty()}>Test reducer</button>
-    </main>
+    <div className="property-columns">
+      <div>
+        {results.map((property) => {
+          return <PropertyCard key={property.id} property={property} />;
+        })}
+      </div>
+      <div>
+        {savedProperties.map((property) => {
+          return <PropertyCard key={property.id} property={property} />;
+        })}
+      </div>
+    </div>
   );
 };
 
