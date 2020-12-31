79d3f40284c1ddbe672c59fedda142679a5d7a82
“REA <me@example.com>
2020-12-31 16:38:12 +1100

    file strcuture udpate

---
+++ src/pages/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <main></main>;
+};
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import Home from "./Home.js";
+import Header from "./Header.js";
+
+export { Home, Header };
