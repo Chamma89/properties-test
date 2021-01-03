3c7097267f4ea4a6513655cc0b59c23ce9a3b06d
“REA <me@example.com>
2021-01-03 19:21:18 +1100

    Test to confirm that all properties are loaded on page

--- src/properties.test.js
+++ src/properties.test.js
@@ -20,8 +20,21 @@ beforeEach(() => {
   );
 });
 
+// Quick demo to check tests are working
+
 test(`Find "Results" heading`, async () => {
   let heading = await screen.findAllByText("Results");
 
   expect(heading).toBeTruthy();
+});
+
+// Check via dataid that all properties on page are visible
+
+const allropertyIds = [1, 2, 3, 4, 5];
+
+allropertyIds.forEach((propId) => {
+  test(`Check if property with dataid={${propId}} exists in document`, async () => {
+    let properties = await screen.findByTestId(propId);
+    expect(properties).toBeInTheDocument();
+  });
 });
